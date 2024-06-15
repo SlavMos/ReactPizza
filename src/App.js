321a85f6bf44cea8fcc099944ea950888dfa9bcf
SlavMos <vladanik1999@gmail.com>
2024-06-15 16:47:00 +0400

    Fetch pizzas from backend and use useEffect

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ import Sort from "./components/Sort.jsx";
 import "./scss/app.scss";
 import React from "react";
 import pizzas from "./components/assets/pizzas.json";
-console.log(pizzas);
 
 function App() {
+  const [items, setItems] = React.useState([]); // для fetch запроса
+
+  React.useEffect(() => {
+    fetch("https://666c15f449dbc5d7145c8874.mockapi.io/items") //когда будет fetch запрос
+      .then((res) => {
+        // тогда перекомвертируй в json формат
+        return res.json();
+      })
+      .then((json) => {
+        //тогда верни данные в setItems
+        setItems(json);
+      });
+  }, []); // чтоб вызвался один раз(componentDidMount)
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
